319a2d2075f503af86a29a3f9b08c48176aeee3e
tinkov <vasy499@mail.ru>
2023-04-20 09:50:15 +0300

    Временно отключил класс TelegramBotUpdatesListener

--- src/main/java/com/example/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/com/example/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -1,55 +1,57 @@
 package com.example.telegrambot.listener;
 
-import ch.qos.logback.core.encoder.EchoEncoder;
-import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.UpdatesListener;
-import com.pengrad.telegrambot.model.Message;
-import com.pengrad.telegrambot.model.Update;
-import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.response.SendResponse;
-import org.slf4j.ILoggerFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+//import ch.qos.logback.core.encoder.EchoEncoder;
+//import com.pengrad.telegrambot.TelegramBot;
+//import com.pengrad.telegrambot.UpdatesListener;
+//import com.pengrad.telegrambot.model.Message;
+//import com.pengrad.telegrambot.model.Update;
+//import com.pengrad.telegrambot.request.SendMessage;
+//import com.pengrad.telegrambot.response.SendResponse;
+//import org.slf4j.ILoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Component;
+//
+//import javax.annotation.PostConstruct;
+//import java.util.List;
 
-import javax.annotation.PostConstruct;
-import java.util.List;
-
-@Component
-public class TelegramBotUpdatesListener implements UpdatesListener {
-
-    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
-    private final TelegramBot telegramBot;
-
-    public TelegramBotUpdatesListener(TelegramBot telegramBot) {
-        this.telegramBot = telegramBot;
-    }
-
-    @PostConstruct
-    public void init() {
-        telegramBot.setUpdatesListener(this);
-    }
-
-    @Override
-    public int process(List<Update> updates) {
-        try {
-            updates.forEach(update -> {
-                logger.info("Processing update: {}", update);
-                Message message = update.message();
-                Long chatId = message.chat().id();
-                String text = message.text();
-
-                if ("/start".equals(text)) {
-                    SendMessage sendMessage = new SendMessage(chatId, "Привет!Это приют кошек и собак");
-                   SendResponse sendResponse = telegramBot.execute(sendMessage);
-                    if (!sendResponse.isOk()) {
-                        logger.error("Error during sending message: {}", sendResponse.description());
-                    }
-                }
-            });
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-        }
-        return UpdatesListener.CONFIRMED_UPDATES_ALL;
-    }
-}
+//@Component
+//public class TelegramBotUpdatesListener implements UpdatesListener {
+//
+//    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+//    private final TelegramBot telegramBot;
+//
+//    public TelegramBotUpdatesListener(TelegramBot telegramBot) {
+//        this.telegramBot = telegramBot;
+//    }
+//
+//    @PostConstruct
+//    public void init() {
+//        telegramBot.setUpdatesListener(this);
+//    }
+//
+//    @Override
+//    public int process(List<Update> updates) {
+//        try {
+//            updates.forEach(update -> {
+//                logger.info("Processing update: {}", update);
+//                Message message = update.message();
+//                Long chatId = message.chat().id();
+//                String text = message.text();
+//
+//                if ("/start".equals(text)) {
+//                    SendMessage sendMessage = new SendMessage(chatId, "Привет!Это приют кошек и собак");
+//                    SendResponse sendResponse = telegramBot.execute(sendMessage);
+//                    if (!sendResponse.isOk()) {
+//                        logger.error("Error during sending message: {}", sendResponse.description());
+//                    }
+//                }
+//            });
+//        } catch (Exception e) {
+//            logger.error(e.getMessage(), e);
+//        }
+//        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+//    }
+//}
+//
+//
