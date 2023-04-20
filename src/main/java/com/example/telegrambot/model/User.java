ec1ed87db4faa3792e631778719f5cac67e7b0b0
tinkov <vasy499@mail.ru>
2023-04-20 09:06:04 +0300

    added model, logs, pom and config commit

---
+++ src/main/java/com/example/telegrambot/model/User.java
@@ -0,0 +1,64 @@
+package com.example.telegrambot.model;
+
+import java.util.Objects;
+
+public class User {
+    private Long id;
+    private String username;
+    private String firstName;
+    private String lastName;
+
+
+    public User() {
+    }
+
+    public User(String username, String firstName, String lastName) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public User(String username, String firstName, String lastName, Long id) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(username, user.username) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, firstName, lastName, id);
+    }
+}
