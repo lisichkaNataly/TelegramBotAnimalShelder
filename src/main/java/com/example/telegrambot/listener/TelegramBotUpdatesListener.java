9543175f71cb1ffd33c4d68635508bfc29db7568
Izmaylova <Natalyatf@yandex.ru>
2023-05-08 09:22:22 +0500

    удалила класс волонтеров, добавила методы в KeyBoardShelter, добавила ReportPetService и методы

--- src/main/java/com/example/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/com/example/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -1,5 +1,11 @@
 package com.example.telegrambot.listener;
 
+import com.example.telegrambot.repository.ReportPetRepository;
+import com.example.telegrambot.repository.UserCatRepository;
+import com.example.telegrambot.repository.UserDogRepository;
+import com.example.telegrambot.repository.UserRepository;
+import com.example.telegrambot.service.KeyBoardShelter;
+import com.example.telegrambot.service.ReportPetService;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Message;
@@ -9,45 +15,96 @@ import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
 import javax.annotation.PostConstruct;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 @Component
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
-    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
-    private final TelegramBot telegramBot;
+        private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+        private final TelegramBot telegramBot;
+    private static final String START_CMD = "/start";
 
-    public TelegramBotUpdatesListener(TelegramBot telegramBot) {
-        this.telegramBot = telegramBot;
-    }
+    private static final String GREETING_TEXT = ", Приветствую! Чтобы найти то, что тебе нужно - нажми на нужную кнопку";
 
-    @PostConstruct
-    public void init() {
-        telegramBot.setUpdatesListener(this);
-    }
+    private static final String infoAboutBot = "Информация о возможностях бота \n- Бот может показать информацию о приюте \n" +
+            "- Покажет какие документы нужны \n- Бот может принимать ежедневный отчет о питомце\n" +
+            "- Может передать контактные данные волонтерам для связи";
+    private static final String infoAboutShelterDog = "Наш сайт с информацией о приюте для собак \nhttps://google.com \n" +
+            "Контактные данные \nhttps://yandex.ru\n" +
+            "Общие рекомендации \nhttps://ru.wikipedia.org\n" +
+            "";
+    private static final String infoAboutShelterCat = "Наш сайт с информацией о приюте для кошек \nhttps://google.com \n" +
+            "Контактные данные \nhttps://yandex.ru\n" +
+            "Общие рекомендации \nhttps://ru.wikipedia.org\n" +
+            "";
+    private static final String infoAboutDogs = "Правила знакомства с животным \nhttps://google.com \n" +
+            "Список документов \nhttps://yandex.ru\n" +
+            "Список рекомендаций \nhttps://ru.wikipedia.org\n" +
+            "Советы кинолога \nhttps://ru.wikipedia.org\n" +
+            "Прочая информация \nhttps://google.com\n" +
+            "";
 
-    @Override
-    public int process(List<Update> updates) {
-        try {
-            updates.forEach(update -> {
-                logger.info("Processing update: {}", update);
-                Message message = update.message();
-                Long chatId = message.chat().id();
-                String text = message.text();
+    private static final String infoAboutCats = "Правила знакомства с животным \nhttps://google.com \n" +
+            "Список документов \nhttps://yandex.ru\n" +
+            "Список рекомендаций \nhttps://ru.wikipedia.org\n" +
+            "Прочая информация \nhttps://google.com\n" +
+            "";
+    private static final String infoContactsVolonter = "Контактные данные волонтера  \n @a1ekseev_artem \n" +
+            "Телефон - +7 999 999 99 99 \n";
+    private static final String infoAboutReport = "Для отчета нужна следующая информация:\n" +
+            "- Фото животного.  \n" +
+            "- Рацион животного\n" +
+            "- Общее самочувствие и привыкание к новому месту\n" +
+            "- Изменение в поведении: отказ от старых привычек, приобретение новых.\nСкопируйте следующий пример. Не забудьте прикрепить фото";
 
-                if ("/start".equals(text)) {
-                    SendMessage sendMessage = new SendMessage(chatId, "Привет!Это приют кошек и собак.");
-                    SendResponse sendResponse = telegramBot.execute(sendMessage);
-                    if (!sendResponse.isOk()) {
-                        logger.error("Error during sending message: {}", sendResponse.description());
-                    }
-                }
-            });
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
+    private static final String reportExample = "Рацион: ваш текст;\n" +
+            "Самочувствие: ваш текст;\n" +
+            "Поведение: ваш текст;";
+
+    private static final String REGEX_MESSAGE = "(Рацион:)(\\s)(\\W+)(;)\n" +
+            "(Самочувствие:)(\\s)(\\W+)(;)\n" +
+            "(Поведение:)(\\s)(\\W+)(;)";
+
+    private static final long telegramChatVolunteers = -748879962L;
+    private long daysOfReports;
+    private ReportPetRepository reportPetRepository;
+    private UserDogRepository userDogRepository;
+    private UserCatRepository userCatRepository;
+    private KeyBoardShelter keyBoardShelter;
+    private ReportPetService reportPetService;
+
+        public TelegramBotUpdatesListener(TelegramBot telegramBot) {
+            this.telegramBot = telegramBot;
         }
-        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+
+        @PostConstruct
+        public void init() {
+            telegramBot.setUpdatesListener(this);
+        }
+
+        @Override
+        public int process(List<Update> updates) {
+            try {
+                updates.forEach(update -> {
+                    logger.info("Processing update: {}", update);
+                    Message message = update.message();
+                    Long chatId = message.chat().id();
+                    String text = message.text();
+
+                    if ("/start".equals(text)) {
+                        SendMessage sendMessage = new SendMessage(chatId, "Привет!Это приют кошек и собак.Выбери меню");
+                        SendResponse sendResponse = telegramBot.execute(sendMessage);
+                        if (!sendResponse.isOk()) {
+                            logger.error("Error during sending message: {}", sendResponse.description());
+                        }
+                    }
+                });
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+            }
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+        }
     }
-}
--- src/main/java/com/example/telegrambot/service/KeyBoardShelter.java
+++ src/main/java/com/example/telegrambot/service/KeyBoardShelter.java
@@ -1,54 +1,87 @@
 package com.example.telegrambot.service;
 
-//import com.example.telegrambot.listener.TelegramBotUpdatesListener;
-//import com.pengrad.telegrambot.TelegramBot;
-//import com.pengrad.telegrambot.model.request.KeyboardButton;
-//import com.pengrad.telegrambot.model.request.ParseMode;
-//import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
-//import com.pengrad.telegrambot.request.SendMessage;
-//import com.pengrad.telegrambot.response.SendResponse;
-//import com.vdurmont.emoji.EmojiParser;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class KeyBoardShelter {
-//
-//    private TelegramBot telegramBot;
-//    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
-//
-//    public void chooseMenu (long chatId){
-//        logger.info("Method sendMessage has been run: {}, {}", chatId, "Вызвано меню выбора ");
-//        String emojiCat = EmojiParser.parseToUnicode(":cat:");
-//        String emojiDog = EmojiParser.parseToUnicode(":dog:");
-//        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(
-//                new KeyboardButton(emojiCat + " CAT "));
-//        replyKeyboardMarkup.addRow(new KeyboardButton(emojiDog + " DOG "));
-//
-//        returnResponseReplyKeyboardMarkup(replyKeyboardMarkup, chatId, "Выберите, кого хотите приютить:");
-//    }
-//
-//    private void returnResponseReplyKeyboardMarkup(
-//            ReplyKeyboardMarkup replyKeyboardMarkup, Long chatId, String text) {
-//
-//        replyKeyboardMarkup.resizeKeyboard(true);
-//        replyKeyboardMarkup.oneTimeKeyboard(false);
-//        replyKeyboardMarkup.selective(false);
-//        SendMessage request = new SendMessage(chatId, text)
-//                .replyMarkup(replyKeyboardMarkup)
-//                .parseMode(ParseMode.HTML)
-//                .disableWebPagePreview(true);
-//        SendResponse sendResponse = telegramBot.execute(request);
-//        if(!sendResponse.isOk()){
-//            int codeError = sendResponse.errorCode();
-//            String description = sendResponse.description();
-//            logger.info("code of error: {}", codeError);
-//            logger.info("description -:", description);
-//        }
-//
-//    }
-//
-//
-//}
+
+import com.example.telegrambot.listener.TelegramBotUpdatesListener;
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.request.KeyboardButton;
+import com.pengrad.telegrambot.model.request.ParseMode;
+import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
+import com.vdurmont.emoji.EmojiParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KeyBoardShelter {
+
+    @Autowired
+    private TelegramBot telegramBot;
+    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+
+    //меню выбора
+    public void chooseMenu(long chatId) {
+        logger.info("Method sendMessage has been run: {}, {}", chatId, "Вызвано меню выбора ");
+        String emojiCat = EmojiParser.parseToUnicode(":cat:");
+        String emojiDog = EmojiParser.parseToUnicode(":dog:");
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(
+                new KeyboardButton(emojiCat + " CAT "));
+        replyKeyboardMarkup.addRow(new KeyboardButton(emojiDog + " DOG "));
+
+        returnResponseReplyKeyboardMarkup(replyKeyboardMarkup, chatId, "Выберите, кого хотите приютить:");
+    }
+
+   //основное меню
+    public void sendMenu(long chatId) {
+        logger.info("Method sendMessage has been run: {}, {}", chatId, "Вызвано основное меню ");
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(
+                new KeyboardButton("Информация о возможностях бота"),
+                new KeyboardButton("Узнать информацию о приюте"));
+        replyKeyboardMarkup.addRow(new KeyboardButton("Как взять питомца из приюта"),
+                new KeyboardButton("Прислать отчет о питомце"));
+        replyKeyboardMarkup.addRow(new KeyboardButton("Позвать волонтера"));
+        returnResponseReplyKeyboardMarkup(replyKeyboardMarkup, chatId, "Главное меню");
+    }
+
+    //меню информации о приюте
+    public void sendMenuInfoShelter(long chatId) {
+        logger.info("Method sendMenuInfoShelter has been run: {}, {}", chatId, "Вызвали Информация о приюте");
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(new KeyboardButton("Информация о приюте"),
+                new KeyboardButton("Оставить контактные данные").requestContact(true));
+        replyKeyboardMarkup.addRow(new KeyboardButton("Позвать волонтера"),
+                new KeyboardButton("Вернуться в меню"));
+        returnResponseReplyKeyboardMarkup(replyKeyboardMarkup, chatId, "Информация о приюте");
+    }
+
+    //меню как взять питомца
+    public void sendMenuTakeAnimal(long chatId) {
+        logger.info("Method sendMenuTakeAnimal has been run: {}, {}", chatId, "вызвали Как взять питомца из приюта");
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(
+                new KeyboardButton("Советы и рекомендации"),
+                new KeyboardButton("Оставить контактные данные").requestContact(true));
+        replyKeyboardMarkup.addRow(new KeyboardButton("Позвать волонтера"),
+                new KeyboardButton("Вернуться в меню"));
+        returnResponseReplyKeyboardMarkup(replyKeyboardMarkup, chatId, "Как взять питомца из приюта");
+    }
+
+    private void returnResponseReplyKeyboardMarkup(
+            ReplyKeyboardMarkup replyKeyboardMarkup, Long chatId, String text) {
+
+        replyKeyboardMarkup.resizeKeyboard(true);
+        replyKeyboardMarkup.oneTimeKeyboard(false);
+        replyKeyboardMarkup.selective(false);
+        SendMessage request = new SendMessage(chatId, text)
+                .replyMarkup(replyKeyboardMarkup)
+                .parseMode(ParseMode.HTML)
+                .disableWebPagePreview(true);
+        SendResponse sendResponse = telegramBot.execute(request);
+        if (!sendResponse.isOk()) {
+            int codeError = sendResponse.errorCode();
+            String description = sendResponse.description();
+            logger.info("code of error: {}", codeError);
+            logger.info("description -:", description);
+        }
+    }
+}
